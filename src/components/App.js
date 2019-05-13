b295f35ade0fdaecf021fb15dc1c202879065ce9
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-05-13 19:13:49 +0200

    switched from SearchBar for ApiData to RadioButtons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Top from './Header';
-import SearchBar from './SearchBar';
+import SearchBar from './RadiobuttonForm';
 import DataList from './DataList';
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
 export default App
 
 // #TODO MAJOR  - show DataList with map
-// #TODO MAJOR  - Switch from Seaarchbar to checkboxes/radioButtons
+// MAJOR  - Switch from Seaarchbar to checkboxes/radioButtons -- DONE
 // #TODO MAJOR  - Add and initialize DetailCards
 
 // #TODO Medium - SearchBar on top of DataOverview to search for names with help of regEX
---
+++ src/components/RadiobuttonForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import axios from 'axios';
+
+
+class SearchBar extends React.Component{
+    state = {selectedOption: 'films'};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.selectedOption);
+        this.getUser(this.state.selectedOption);
+    };
+
+    getUser = async (term) => {
+        try {
+            const response = await axios.get('https://swapi.co/api/' + term);
+            console.log(response.data.results);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    render(){
+    return (
+        <div className="ui form">
+            <form onSubmit={this.onFormSubmit}>
+                <p>May the force be with you!</p>
+                <fieldset>
+                    {/*<legend>May the force be with you!</legend>*/}
+                    <input  id="fm" type="radio" name="choice" defaultChecked="defaultChecked"
+                            value="films"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="fm">Films</label>
+                    <input  id="pl" type="radio" name="choice"
+                            value="people/"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="pl">People</label>
+                    <input  id="pla" type="radio" name="choice"
+                            value="planets"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="pla">Planets</label>
+
+                    <br />
+
+                    <input  id="sp" type="radio" name="choice"
+                            value="species"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="sp">Species</label>
+                    <input  id="st" type="radio" name="choice"
+                            value="starships"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="st">Starships</label>
+                    <input  id="ve" type="radio" name="choice"
+                            value="vehicles"
+                            onChange={ (e) => { this.setState({selectedOption : e.target.value})} }/>
+                    <label htmlFor="ve">Vehicles</label>
+                </fieldset>
+                <button className="ui primary button">Chose</button>
+            </form>
+        </div>
+    )
+}}
+
+export default SearchBar
