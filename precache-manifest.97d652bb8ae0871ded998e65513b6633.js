88efc3ff32382eccfee37ab58484e7d5dadc80b6
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-05-18 12:42:59 +0200

    Updates

---
+++ precache-manifest.97d652bb8ae0871ded998e65513b6633.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa44fb26674f4002836fc7977681c21",
+    "url": "/swapiapi/index.html"
+  },
+  {
+    "revision": "7aa40b8c3f45328814e4",
+    "url": "/swapiapi/static/css/main.6c9a56d7.chunk.css"
+  },
+  {
+    "revision": "5e1585b46156afc30d1b",
+    "url": "/swapiapi/static/js/2.8633e734.chunk.js"
+  },
+  {
+    "revision": "7aa40b8c3f45328814e4",
+    "url": "/swapiapi/static/js/main.4de3556f.chunk.js"
+  },
+  {
+    "revision": "31560792bac3240a6680",
+    "url": "/swapiapi/static/js/runtime~main.9d2c3e58.js"
+  }
+]);
